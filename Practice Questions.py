0bfc1c0fda204044b6dfd492c70f4490c54f3410
Manas <tenshoukohaku2005@gmail.com>
2023-01-21 20:28:02 +0530

    For Loop and a pattern

--- Practice Questions.py
+++ Practice Questions.py
@@ -1,11 +1,5 @@
-n = int(input("Enter the Maximum range of the Triangular Pattern : "))
-for i in range(1,n) : 
-    for j in range(i) : 
-        print("*",end = " ")
-    print(" ")
-i = 1
-j = 1
-for i in range(n,0,-1) : 
-    for j in range(i) : 
-        print("*",end = " ")
-    print(" ")
+a = list()
+for i in range(4,31) :
+    if i%2 == 0 : 
+        b = a.append(i)
+print(a)
