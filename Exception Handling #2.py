63f2fe0f04e9d4c67f56dde36959bb175af269be
Manas <tenshoukohaku2005@gmail.com>
2023-05-03 23:22:39 +0530

    Raise Exception

---
+++ Exception Handling #2.py
@@ -0,0 +1,15 @@
+# num = [2,6,3,4]
+# print("Sample Test")
+# if len(num) > 2 : 
+#     print("Hello")
+#     if len(num) > 3 : 
+#         raise Exception("Length Should Be Greater Than 3")
+# print("Nice To Meet You")
+
+# Sample Test 2
+age = int(input("Enter the Age of the User : "))
+if age >= 18 : 
+    print("The User is Eligible For Voting")
+else : 
+    raise Exception("The User is Not Eligible For Voting")
+print("Vote With Honesty And Wait For The Result")
