97d2e053c1a88716a37a5a6c7fe150ef9c511963
Manas <tenshoukohaku2005@gmail.com>
2023-05-05 00:57:51 +0530

    Practice

--- Practice Questions.py
+++ Practice Questions.py
@@ -1,5 +1,8 @@
-n = int(input("Enter the Range of the Addition : "))
-sum = 0
-for i in range(1,n+1) : 
-    sum = sum + i
-print("The Sum of All n Natural Number is : ",sum)
+list1 = [12,75,150,180,145,525,50]
+for i in range(len(list1)) : 
+    if list1[i] > 500 : 
+        break
+    elif list1[i] > 150 : 
+        continue
+    elif list1[i]%5 == 0 : 
+    	print(list1[i])
