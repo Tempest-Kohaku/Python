0dfd851fd198b8c9e781d99933fbd9cbe69dbf99
Manas <tenshoukohaku2005@gmail.com>
2023-05-04 23:58:11 +0530

    Practice

--- Practice Questions.py
+++ Practice Questions.py
@@ -1,5 +1,5 @@
-n = int(input("Enter the Number of rows for the Pattern : "))
+n = int(input("Enter the Range of the Addition : "))
+sum = 0
 for i in range(1,n+1) : 
-    for j in range(1,i+1) : 
-        print(j, end = " ")
-    print("")
+    sum = sum + i
+print("The Sum of All n Natural Number is : ",sum)
