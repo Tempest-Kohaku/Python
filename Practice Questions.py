5ac8af2f278a391772cc53dc807794bffaad4836
Manas <tenshoukohaku2005@gmail.com>
2023-05-05 01:03:31 +0530

    Moreee

--- Practice Questions.py
+++ Practice Questions.py
@@ -1,8 +1,5 @@
-list1 = [12,75,150,180,145,525,50]
-for i in range(len(list1)) : 
-    if list1[i] > 500 : 
-        break
-    elif list1[i] > 150 : 
-        continue
-    elif list1[i]%5 == 0 : 
-    	print(list1[i])
+list1 = [10,20,30,40,50]
+print("The Reverse of The List is : ")
+for i in range(len(list1),0,-1) : 
+    i = i -1
+    print(list1[i],end = " ")
