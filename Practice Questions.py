1f46d6ff2b868538369ede499b318c1e2e1dd00b
Manas <tenshoukohaku2005@gmail.com>
2023-05-08 23:44:04 +0530

    More Practice

--- Practice Questions.py
+++ Practice Questions.py
@@ -1,8 +1,4 @@
-n = int(input("Enter the Number : "))
-tmp = n
-reverse = 0
-while n > 0 : 
-    x = n%10
-    reverse = (reverse * 10) + x
-    n = n//10
-print(f"The Reverse of {tmp} is {reverse}")
+a = list()
+for i in range(4,31) : 
+    a.append(i)
+print(a)
