a60579d76b7796828f2f2541ce5b000980fa6614
Manas <tenshoukohaku2005@gmail.com>
2023-05-09 00:03:10 +0530

    Final Day Practice

--- Practice Questions.py
+++ Practice Questions.py
@@ -1,4 +1,13 @@
-a = list()
-for i in range(4,31) : 
-    a.append(i)
-print(a)
+list1 = list()
+n = int(input("Enter the Number of the Elements in the List : "))
+for i in range(n) : 
+    a = int(input("Enter the Number to be Added in the List : "))
+    list1.append(a)
+maximum = list1[0]
+minimum = list1[0]
+for j in list1 : 
+    if maximum < j : 
+        maximum = j
+    if minimum > j : 
+        minimum = j
+print(f"The Greatest Number is {maximum} and Smallest number is {minimum}")
